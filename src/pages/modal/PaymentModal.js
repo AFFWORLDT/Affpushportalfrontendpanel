cff501ab6a26576143e162e25ea299f945c36d1e
Affworld Technologies PVT Ltd <123453482+AFFWORLDT@users.noreply.github.com>
2024-01-30 04:14:42 -0800

    Update PaymentModal.js

--- src/pages/modal/PaymentModal.js
+++ src/pages/modal/PaymentModal.js
@@ -6,25 +6,25 @@ import "./paymentmodel.css";
 
 function PaymentModal({ showModal, hideModal, projects, projectInfo }) {
   return (
-    <div className='modelpayment'>
-      <Modal show={showModal} onHide={hideModal} style={{ width: "100vw" }}>
+    <div className='modelpaymen '>
+      <Modal  show={showModal} onHide={hideModal} className='model-container'>
         <Modal.Header closeButton>
-          <Modal.Title className="fw-bold">All Payment Details</Modal.Title>
+          <Modal.Title className="fw-bold flex align text-primary">All Payment Details</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {projectInfo ? (
             <>
               {projects.map((data, index) => (
                 <div key={index} className='modelpayment'>
-                  <span className='fs-5 fw-bold'>Payment Details-{index + 1}</span>
-                  <p style={{ fontWeight: 750 }} className='fs-5 '>Beneficiary Name: <span style={{ fontWeight: 600 }}>{data.beneficiary_name}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Account Number:<span style={{ fontWeight: 600 }}>  {data.account_number}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Account Type: <span style={{ fontWeight: 600 }}>{data.account_type}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Bank Name: <span style={{ fontWeight: 600 }}>{data.bank_name}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Bank Address: <span style={{ fontWeight: 600 }}>{data.bank_address}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Sort Code: <span style={{ fontWeight: 600 }}>{data.sort_code}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>SWIFT: <span style={{ fontWeight: 600 }}> {data.swift}</span></p>
-                  <p style={{ fontWeight: 750 }} className='fs-5'>Iban Code: <span style={{ fontWeight: 600 }}>{data.iban}</span></p>
+                  <span className='fs-5 fw-bold'>Payment Details - {index + 1}</span>
+                  <p style={{ fontWeight: 750 }} className='fs-5 mt-2'>Beneficiary Name : <span style={{ fontWeight: 600 }}>{data.beneficiary_name}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Account Number :<span style={{ fontWeight: 600 }}>  {data.account_number}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Account Type : <span style={{ fontWeight: 600 }}>{data.account_type}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Bank Name : <span style={{ fontWeight: 600 }}>{data.bank_name}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Bank Address : <span style={{ fontWeight: 600 }}>{data.bank_address}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Sort Code : <span style={{ fontWeight: 600 }}>{data.sort_code}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>SWIFT : <span style={{ fontWeight: 600 }}> {data.swift}</span></p>
+                  <p style={{ fontWeight: 750 }} className='fs-5'>Iban Code : <span style={{ fontWeight: 600 }}>{data.iban}</span></p>
                   <hr />
                 </div>
               ))}
