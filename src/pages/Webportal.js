c56f1a29b16edf48bf8c5f0b470270dfdad4e347
rahul <affworldtechnologies@gmail.com>
2024-01-30 19:10:56 +0530

    rahul

--- src/pages/Webportal.js
+++ src/pages/Webportal.js
@@ -145,7 +145,7 @@ export default function DashboardAppPage() {
             item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="Listed sites" total={203} color="info" icon={'ant-design:interaction-filled'} />
           </Grid>
-          <Grid style={{ cursor: 'pointer' }} onClick={() => navigate("/affilate/user/Pushportal")}
+          <Grid style={{ cursor: 'pointer' }} onClick={() => navigate("/affilate/pushportal")}
             item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="PushPortal" total={1352831}  icon={'ant-design:thunderbolt-outlined'} />
           </Grid>
