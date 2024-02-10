5bef848c395c37aa6a9915b57d04df0430d2dc08
Affworld Technologies PVT Ltd <123453482+AFFWORLDT@users.noreply.github.com>
2024-02-10 13:50:26 +0530

    Update Adspace.js

--- src/pages/Adspace.js
+++ src/pages/Adspace.js
@@ -228,7 +228,7 @@ export default function DashboardAppPage() {
           
 <Box sx={{ width: '20%', height: '60vh', display: 'block' }}>
             <iframe
-              src="https://affiliate-api.affworld.cloud/api/misc/iframe_code?affiliate_id=4b5562fb788d4cdf99b0dcd5d461a8b2&iframe_id=1&iframe_type=main"
+              src="'https://affiliate-api.affworld.cloud/api/misc/iframe_code?affiliate_id=4b5562fb788d4cdf99b0dcd5d461a8b2&iframe_id=1&iframe_type=popup"
               scrolling="no"
               style={{ border: 'none', width: '100%', height: '100%' }}
             />
