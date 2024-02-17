718bd49cd552f8b005c9757672662736dda85ff5
Affworld Technologies PVT Ltd <123453482+AFFWORLDT@users.noreply.github.com>
2024-02-17 08:59:32 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -112,25 +112,25 @@ export default function LoginPage() {
               <Link to="/register" variant="subtitle2" style={{ textDecoration: 'none' }} underline="hover"><b>Get started</b></Link>
             </Typography>
 
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
+            // <Stack direction="row" spacing={2}>
+            //   <Button fullWidth size="large" color="inherit" variant="outlined">
+            //     <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
+            //   </Button>
 
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
+            //   <Button fullWidth size="large" color="inherit" variant="outlined">
+            //     <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
+            //   </Button>
 
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
+            //   <Button fullWidth size="large" color="inherit" variant="outlined">
+            //     <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
+            //   </Button>
+            // </Stack>
 
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
-              </Typography>
-            </Divider>
+            // <Divider sx={{ my: 3 }}>
+            //   <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            //     OR
+            //   </Typography>
+            // </Divider>
 
             <LoginForm />
           </StyledContent>
